7c3fc98a69563a5d68d17836600268f1071e6543
diokou_tech <diokoucheikh@gmail.com>
2022-08-26 10:13:56 +0000

    update login form

--- src/app/frontoffice/components/login/login.component.ts
+++ src/app/frontoffice/components/login/login.component.ts
@@ -8,8 +8,11 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 export class LoginComponent implements OnInit, OnDestroy {
 
   constructor() { }
-
+  inputClass="border-3 focus:border-4 w-full transition ease-in-out focus:outline-none p-3 border rounded-5 border-cyan-400";  
   ngOnInit(): void {
+  }
+  login(){
+    alert('test');
   }
 
   ngOnDestroy(): void {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   exports:[
     MatIconModule,
     MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   declarations: [],
   imports: [
